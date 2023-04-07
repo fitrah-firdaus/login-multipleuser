bbd973390be005e09d392defd874838a546629d7
ZakiAndriansa <andriansazaki1@gmail.com>
2023-04-07 10:52:13 +0700

    Menambahkan if condition

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<?php
+
+    $user = "user";
+
+    if($user == "user"){
+        $ghost = "ghost";
+    } if($user == "admin"){
+        $ghost = "";
+    }
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,6 +17,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Halaman Utama</title>
+    <style>
+        .ghost{
+            display: none;
+        }
+
+        tr, th, td{
+            border: 1px black solid;
+        }
+    </style>
 </head>
 <body>
     <table>
@@ -12,13 +33,13 @@
             <th>Nama</th>
             <th>Kelas</th>
             <th>No Absen</th>
-            <th>Aksi</th>
+            <th class=<?= $ghost ?>>Aksi</th>
         </tr>
         <tr>
             <td>Zaki Andriansa</td>
             <td>10 PPLG 1</td>
             <td>42</td>
-            <td>
+            <td class=<?= $ghost ?>>
                 <a href="#">Edit</a>
                 <a href="#">Hapus</a>
             </td>
