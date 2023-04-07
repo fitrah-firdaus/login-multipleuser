dad4e0e14610934b9c05f66b93e8a1b65f36bbac
ZakiAndriansa <andriansazaki1@gmail.com>
2023-04-07 11:14:20 +0700

    membuat file registrasi.php

---
+++ registrasi.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Registrasi</title>
+</head>
+<body>
+    <h1>Halaman Registrasi</h1>
+
+    <form action="" method="post">
+        <label for="username">Username : </label>
+        <input type="text" name="username" id="username">
+
+        <br>
+
+        <label for="password">Password : </label>
+        <input type="password" name="password" id="password">
+
+        <br>
+
+        <label for="cpassword">Confirm password : </label>
+        <input type="cpassword" name="cpassword" id="cpassword">
+
+        <br>
+
+        <button type="submit">Login!</button>
+    </form>
+</body>
+</html>
